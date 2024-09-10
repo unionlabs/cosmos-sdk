edc38d382945c3481eda54eec622622edcace86a
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-09-10 16:54:13 -0500

    feat: use flake parts and flake modules

---
+++ flake.nix
@@ -0,0 +1,185 @@
+{
+  description = "A Framework for Building High Value Public Blockchains";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    swagger-combine-src = {
+      flake = false;
+      url = "github:maxdome/swagger-combine";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cosmosproto = {
+      url = "github:cosmos/cosmos-proto?rev=3f5f5eb19538f5ff023e88461fb7ca890807f5e1";
+      flake = false;
+    };
+    gogoproto = {
+      url = "github:cosmos/gogoproto?rev=b12c8cae0624d2518ab995c775410694dfa5d50e";
+      flake = false;
+    };
+    googleapis = {
+      url = "github:googleapis/googleapis?rev=6774ccbbc3f182f6ae3a32dca29e1da489ad8a8f";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, treefmt-nix, cosmosproto, gogoproto, googleapis, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" ];
+      imports = [
+        ./client/v2/clientv2.nix
+        ./proto/proto.nix
+        ./simapp/simapp.nix
+        ./tests/tests.nix
+        treefmt-nix.flakeModule
+      ];
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }:
+        let
+          mkUnpack = import ./tools/mkUnpack.nix { inherit pkgs; };
+        in
+        {
+          _module = {
+            args = {
+              inherit nixpkgs;
+
+              pkgs = import nixpkgs {
+                inherit system;
+              };
+
+              rev =
+                if (builtins.hasAttr "rev" self') then self'.rev else "dev";
+
+              proto = {
+                cosmossdk = builtins.path {
+                  name = "cosmos-sdk";
+                  path = ./.;
+                };
+                cosmosproto = mkUnpack {
+                  name = "cosmos-proto";
+                  package = cosmosproto;
+                };
+                gogoproto = mkUnpack {
+                  name = "gogoproto";
+                  package = gogoproto;
+                };
+                googleapis = mkUnpack {
+                  name = "googleapis";
+                  package = googleapis;
+                };
+              };
+            };
+          };
+          packages = {
+            go-mod-vendor = pkgs.writeShellApplication {
+              name = "go-mod-vendor";
+              runtimeInputs = with pkgs; [ go ];
+              text = ''
+                # If the current directory contains flake.nix, then we are at the repository root
+                if [[ -f flake.nix ]]
+                then
+                  echo "We are at the repository root. Running script..."
+                else
+                  echo "We are NOT at the repository root. Please cd to the repository root and try again."
+                  exit 1
+                fi
+                for project in \
+                  ./. \
+                  ./client/v2/ \
+                  ./simapp/ \
+                  ./tests/
+                do
+                  echo "Vendoring '$project'..."
+                  (
+                    cd $project;
+                    rm -rf vendor/*;
+                    go mod tidy;
+                    go mod vendor;
+                  )
+                done
+                
+                echo "Done vendoring!"
+              '';
+            };
+            swagger-combine = pkgs.buildNpmPackage {
+              pname = "swagger-combine";
+              version = "10.0.9";
+              src = inputs.swagger-combine-src;
+              dontNpmBuild = true;
+              npmDepsHash = "sha256-FZR8hefkqTwSZJMX4lzS4zk7iGXi0+zi0ol1ia3iLYs=";
+            };
+          };
+
+          packages = {
+            default = self'.packages.simapp;
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              buf
+              go_1_23
+              gopls
+              gotestsum
+              go-tools
+              gotools
+              jq
+              marksman
+              nil
+              nixfmt
+              nodejs
+              openssl
+              protobuf
+              yarn
+              yq
+            ];
+            nativeBuildInputs = [ config.treefmt.build.wrapper ];
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+            SWAGGER_BIN = "${self'.packages.swagger-combine}/bin/swagger-combine";
+            GOPRIVATE = "github.com/unionlabs/*";
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixpkgs-fmt.enable = true;
+            programs.gofumpt.enable = true;
+            settings.global.excludes = [ "./vendor*" "*.git*" "./client/docs/statik/statik.go" "./test/mocks/*" "*.pb.go" "*.pb.gw.go" "*.pulsar.go" "./crypto/keys/secp256k1/*" ];
+          };
+
+          checks = {
+            go-test = pkgs.go.stdenv.mkDerivation {
+              name = "go-test";
+              buildInputs = [ pkgs.go pkgs.gotestsum pkgs.git pkgs.which ];
+              src = ./.;
+              doCheck = true;
+              dontInstall = true;
+              dontBuild = true;
+              checkPhase = ''
+                # Go will try to create a .cache/ dir in $HOME.
+                # We avoid this by setting $HOME to the builder directory
+                export HOME=$(pwd)
+                export GOFLAGS="-mod=vendor -tags=\"cgo,ledger,test_ledger_mock,norace\""
+                export GOPRIVATE="github.com/unionlabs/*";
+                export GOWORK="off"
+
+                go version
+
+                echo "Starting tests for github.com/cosmos/cosmos-sdk"
+
+                gotestsum  ./...
+
+                echo "Finished running sub module tests."
+
+                touch $out
+              '';
+            };
+          };
+        };
+    };
+}
+
---
+++ simapp/simapp.nix
@@ -0,0 +1,55 @@
+{ ... }: {
+  perSystem = { pkgs, lib, self', system, rev, ... }: {
+    packages = {
+      simapp = 
+      let
+        pname = "simd";
+        version = "v0.0.1";
+        tags = [ "ledger" "netgo" "rocksdb" "grocksdb_no_link" ];
+        ldflags = [
+          "-X github.com/cosmos/cosmos-sdk/version.Name=${pname}"
+          "-X github.com/cosmos/cosmos-sdk/version.AppName=${pname}"
+          "-X github.com/cosmos/cosmos-sdk/version.Version=${version}"
+          "-X github.com/cosmos/cosmos-sdk/version.BuildTags=${lib.concatStringsSep "," tags}"
+          "-X github.com/cosmos/cosmos-sdk/version.Commit=${rev}"
+        ];
+        cgo_ldflags =
+          if static then "-lrocksdb -pthread -lstdc++ -ldl -lzstd -lsnappy -llz4 -lbz2 -lz"
+          else if pkgs.stdenv.hostPlatform.isWindows then "-lrocksdb-shared"
+          else "-lrocksdb -pthread -lstdc++ -ldl";
+
+        static = pkgs.stdenv.hostPlatform.isStatic;
+      in
+      pkgs.buildGo123Module {
+        inherit ldflags;
+        name = pname;
+        src = ./.;
+        vendorHash = null;
+        doCheck = true;
+        checkPhase = ''
+          export HOME=$(pwd)
+          export CGO_LDFLAGS="${cgo_ldflags}"
+          # export GOFLAGS="-mod=vendor -tags=\"cgo,ledger,test_ledger_mock,norace\""
+          export GOFLAGS="-mod=vendor"
+          export GOWORK="off"
+
+          echo "Running test for ${pname}"
+          go version
+
+          go test ./...
+
+          echo "Finished running ${pname} tests."
+        '';
+        buildTags = [
+          "netgo"
+          "ledger"
+        ];
+        overrideModAttrs = (_: {
+          GOPRIVATE = "github.com/unionlabs/*";
+          GOFLAGS = "-mod=vendor";
+          GOWORK = "off";
+        });
+      };
+    };
+  };
+}
