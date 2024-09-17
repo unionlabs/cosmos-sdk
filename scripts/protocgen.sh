1fde338be5ad731049b120f97584b38605182544
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-09-17 10:47:11 -0500

    feat: update proto scrips

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -5,18 +5,19 @@
 # docker run --rm -v $(pwd):/workspace --workdir /workspace cosmossdk-proto sh ./scripts/protocgen.sh
 
 echo "Formatting protobuf files"
-find ./ -name "*.proto" -exec clang-format -i {} \;
+find ./ -name "*.proto" -not -path '*/vendor/*' -exec clang-format -i {} \;
 
 set -e
 
 home=$PWD
 
 echo "Generating proto code"
-proto_dirs=$(find ./ -name 'buf.yaml' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./ -name 'buf.yaml' -not -path '*/vendor/*' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   echo "Generating proto code for $dir"
 
   cd $dir
+  echo $(pwd)
   # check if buf.gen.pulsar.yaml exists in the proto directory
   if [ -f "buf.gen.pulsar.yaml" ]; then
     buf generate --template buf.gen.pulsar.yaml
@@ -29,7 +30,7 @@ for dir in $proto_dirs; do
 
   # check if buf.gen.gogo.yaml exists in the proto directory
   if [ -f "buf.gen.gogo.yaml" ]; then
-      for file in $(find . -maxdepth 8 -name '*.proto'); do
+      for file in $(find . -maxdepth 8 -name '*.proto' -not -path '*/vendor/*'); do
         # this regex checks if a proto file has its go_package set to cosmossdk.io/api/...
         # gogo proto files SHOULD ONLY be generated if this is false
         # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
