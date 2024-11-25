0978710d295bc72fdce36db175654bb7ac546c53
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-11-25 11:25:19 -0600

    fix: more bn254 defaults

--- x/genutil/client/cli/collect.go
+++ x/genutil/client/cli/collect.go
@@ -63,7 +63,7 @@ func CollectGenTxsCmd(validator func([]transaction.Msg) error) *cobra.Command {
 			return displayInfo(cmd.ErrOrStderr(), toPrint)
 		},
 	}
-	cmd.Flags().String(FlagConsensusKeyAlgo, "ed25519", "algorithm to use for the consensus key")
+	cmd.Flags().String(FlagConsensusKeyAlgo, "bn254", "algorithm to use for the consensus key")
 	cmd.Flags().String(flagGenTxDir, "", "override default \"gentx\" directory from which collect and execute genesis transactions; default [--home]/config/gentx/")
 
 	return cmd
